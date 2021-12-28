b2ab2f502e07a6be540575e18530f9eb1dac84d0
meassadkhan <85921482+meassadkhan@users.noreply.github.com>
2021-12-28 12:02:44 +0500

    Updated (location,username,individual restaurant)

---
+++ app/src/main/java/com/example/fst_cloud_new/MAPS/Map_Vendor_shop.java
@@ -0,0 +1,209 @@
+package com.example.fst_cloud_new.MAPS;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.fst_cloud_new.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+
+public class Map_Vendor_shop extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private DatabaseReference databaseReference;
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private final long MIN_TIME = 1000;
+    private final long MIN_DIST = 5;
+
+    private EditText editTextLatitude;
+    private EditText editTextLongitude;
+    private EditText location_name;
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_vendor);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_vendor);
+        mapFragment.getMapAsync(this);
+
+
+
+
+
+
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                PackageManager.PERMISSION_GRANTED);
+
+
+        editTextLatitude = findViewById(R.id.editText);
+        editTextLongitude = findViewById(R.id.editText2);
+        location_name = findViewById(R.id.location_name);
+        button = findViewById(R.id.button);
+
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                databaseReference.child("latitude").push().setValue(editTextLatitude.getText().toString());
+                databaseReference.child("longitude").push().setValue(editTextLongitude.getText().toString());
+                databaseReference.child("Title").push().setValue(location_name.getText().toString());
+
+            }
+        });
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Shop Location");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    String databaseLatitudeString = dataSnapshot.child("latitude").getValue().toString().substring(1, dataSnapshot.child("latitude").getValue().toString().length() - 1);
+                    String databaseLongitudedeString = dataSnapshot.child("longitude").getValue().toString().substring(1, dataSnapshot.child("longitude").getValue().toString().length() - 1);
+
+                    String[] stringLat = databaseLatitudeString.split(", ");
+                    Arrays.sort(stringLat);
+                    String latitude = stringLat[stringLat.length - 1].split("=")[1];
+
+                    String[] stringLong = databaseLongitudedeString.split(", ");
+                    Arrays.sort(stringLong);
+                    String longitude = stringLong[stringLong.length - 1].split("=")[1];
+
+
+                    LatLng latLng = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location_name.getText().toString()));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    //       location_name.setText(null);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (mMap != null) {
+
+            {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            }
+
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    try {
+                        editTextLatitude.setText(Double.toString(location.getLatitude()));
+                        editTextLongitude.setText(Double.toString(location.getLongitude()));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+
+            try {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fst_cloud_new/Verification/verification_model.kt
@@ -0,0 +1,16 @@
+package com.example.fst_cloud_new.Verification
+
+class verification_model {
+
+
+    var isVerified =""
+    var name =""
+
+    constructor()
+
+    constructor(name:String,isVerified:String){
+
+        this.isVerified = isVerified
+        this.name = name
+    }
+}
